eeb16760295e99c2f05a783ad4bfa0beed88e7ca
95anuragrr <64358831+95anuragrr@users.noreply.github.com>
2020-09-12 00:06:31 +0530

    Create faultycalculator.py

---
+++ faultycalculator.py
@@ -0,0 +1,23 @@
+# faulty calculator
+operator = (input("Enter the operator:- "))
+val1 = int(input("Enter the first operand ="))
+val2 = int(input("Enter the second operand ="))
+
+if operator == "+":
+    if val1 == 56 and val2 == 9:
+        print("77")
+    else:
+        print("Sum is:-", val1 + val2)
+if operator == "-":
+    print("Substract:-", val1 - val2)
+if operator == "*":
+    if val1 == 45 and val25 == 3:
+        print("555")
+    else:
+        print("Multiply is:-", val1 * val2)
+if operator == "/":
+    if val1 == 56 and val2 == 6:
+        print("4")
+    else:
+        print("Divide is:-", float(val1/val2))
+
